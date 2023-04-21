fb986e3881382078f4fdae7ec78b7d7149f4b181
mzzsfy <mzzsfy@163.com>
2023-04-21 15:53:10 +0800

    update test

--- seq/parallel_test.go
+++ seq/parallel_test.go
@@ -9,8 +9,8 @@ import (
 func Test_Parallel(t *testing.T) {
     FromIntSeq().Take(10).Parallel().ForEach(func(i int) {
         n := 30 + rand.Intn(1000)
-        duration := time.Millisecond * 2000
-        concurrent := 1 + rand.Intn(n-1)/(rand.Intn(10)+2)
+        duration := time.Millisecond * 800
+        concurrent := FromT(rand.Intn(n-1)/(rand.Intn(10)+2), n/10+1, n/2-1).Sort(LessT[int]).Drop(1).FirstOr(0)
         p := NewParallel(concurrent)
         now := time.Now()
         t.Logf("%d,开始,concurrent=%d,n=%d", i, concurrent, n)
