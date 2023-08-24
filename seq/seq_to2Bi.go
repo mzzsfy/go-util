f3d2b7c2c76be2578aee3488e6775493b7361036
mzzsfy <mzzsfy@163.com>
2023-08-24 10:49:43 +0800

    seq add function MapBi2XXX()

---
+++ seq/seq_to2Bi.go
@@ -0,0 +1,51 @@
+package seq
+
+// MapBi2AnyAny 每个元素获取两个值,并转换为 BiSeq
+func (t Seq[T]) MapBi2AnyAny(f func(T) (any, any)) BiSeq[any, any] {
+    return BiFrom(func(f1 func(any, any)) {
+        t(func(t T) {
+            k, v := f(t)
+            f1(k, v)
+        })
+    })
+}
+
+// MapBi2IntString 每个元素获取两个值,并转换为 BiSeq
+func (t Seq[T]) MapBi2IntString(f func(T) (int, string)) BiSeq[int, string] {
+    return BiFrom(func(f1 func(int, string)) {
+        t(func(t T) {
+            k, v := f(t)
+            f1(k, v)
+        })
+    })
+}
+
+// MapBi2StringString 每个元素获取两个值,并转换为 BiSeq
+func (t Seq[T]) MapBi2StringString(f func(T) (string, string)) BiSeq[string, string] {
+    return BiFrom(func(f1 func(string, string)) {
+        t(func(t T) {
+            k, v := f(t)
+            f1(k, v)
+        })
+    })
+}
+
+// MapBi2IntAny 每个元素获取两个值,并转换为 BiSeq
+func (t Seq[T]) MapBi2IntAny(f func(T) (int, any)) BiSeq[int, any] {
+    return BiFrom(func(f1 func(int, any)) {
+        t(func(t T) {
+            k, v := f(t)
+            f1(k, v)
+        })
+    })
+}
+
+// MapBi2StringAny 每个元素获取两个值,并转换为 BiSeq
+func (t Seq[T]) MapBi2StringAny(f func(T) (string, any)) BiSeq[string, any] {
+    return BiFrom(func(f1 func(string, any)) {
+        t(func(t T) {
+            k, v := f(t)
+            f1(k, v)
+        })
+    })
+}
