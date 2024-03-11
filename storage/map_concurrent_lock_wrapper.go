fc18bf5c09d13f52e7454479b21a9581b6a90ce8
mzzsfy <mzzsfy@163.com>
2024-03-11 17:36:17 +0800

    添加MapTypeConcurrentLockWrapper

---
+++ storage/map_concurrent_lock_wrapper.go
@@ -0,0 +1,79 @@
+package storage
+
+import (
+    "sync"
+    _ "unsafe"
+)
+
+type rwWrapper[K comparable, V any] struct {
+    lock sync.RWMutex
+    m    Map[K, V]
+}
+
+func (m *rwWrapper[K, V]) Get(key K) (V, bool) {
+    m.lock.RLock()
+    defer m.lock.RUnlock()
+    return m.m.Get(key)
+}
+
+func (m *rwWrapper[K, V]) GetSimple(key K) (value V) {
+    value, _ = m.Get(key)
+    return
+}
+
+func (m *rwWrapper[K, V]) Has(key K) bool {
+    m.lock.RLock()
+    defer m.lock.RUnlock()
+    return m.m.Has(key)
+}
+
+func (m *rwWrapper[K, V]) Delete(key K) {
+    m.lock.Lock()
+    defer m.lock.Unlock()
+    m.m.Delete(key)
+}
+
+func (m *rwWrapper[K, V]) Put(key K, value V) {
+    m.lock.Lock()
+    defer m.lock.Unlock()
+    m.m.Put(key, value)
+}
+func (m *rwWrapper[K, V]) Clean() {
+    m.lock.Lock()
+    defer m.lock.Unlock()
+    m.m.Clean()
+}
+
+func (m *rwWrapper[K, V]) Count() int {
+    m.lock.RLock()
+    defer m.lock.RUnlock()
+    return m.m.Count()
+}
+
+func (m *rwWrapper[K, V]) Iter(cb func(k K, v V) (stop bool)) bool {
+    m.lock.RLock()
+    defer m.lock.RUnlock()
+    return m.m.Iter(cb)
+}
+
+func (m *rwWrapper[K, V]) IterDelete(cb func(k K, v V) (del bool, stop bool)) bool {
+    var delKeys []K
+    r := m.Iter(func(k K, v V) bool {
+        del, stop := cb(k, v)
+        if del {
+            delKeys = append(delKeys, k)
+        }
+        return stop
+    })
+    for _, k := range delKeys {
+        m.Delete(k)
+    }
+    return r
+}
+
+// MapTypeConcurrentLockWrapper 轻量级的并发包装
+func MapTypeConcurrentLockWrapper[K comparable, V any](m MakeMap[K, V]) MakeMap[K, V] {
+    return MapImpl[K, V](func() Map[K, V] {
+        return &rwWrapper[K, V]{m: m.createMap()}
+    })
+}
