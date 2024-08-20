44e4bc4d7722f0c1bb9100a09a3f6be59de8a764
mzzsfy <mzzsfy@163.com>
2024-08-20 16:04:55 +0800

    添加CacheWrap

--- storage/cache.go
+++ storage/cache.go
@@ -1,6 +1,9 @@
 package storage
 
-import "time"
+import (
+    "sync"
+    "time"
+)
 
 type Cache[K comparable, V any] interface {
     Get(key K) (V, bool)
@@ -16,4 +19,26 @@ type TimedCache[K comparable, V any] interface {
     TTL(key K) time.Duration
 }
 
-//todo
+type CacheWrap[K comparable, V any] struct {
+    lock sync.Mutex
+    Cache[K, V]
+}
+
+func (c *CacheWrap[K, V]) GetOr(key K, def func() V) V {
+    get, b := c.Cache.Get(key)
+    if b {
+        return get
+    }
+    c.lock.Lock()
+    defer c.lock.Unlock()
+    if get, b = c.Cache.Get(key); b {
+        return get
+    }
+    v := def()
+    c.Cache.Set(key, v)
+    return v
+}
+
+func NewCacheWrap[K comparable, V any](cache Cache[K, V]) *CacheWrap[K, V] {
+    return &CacheWrap[K, V]{Cache: cache}
+}
