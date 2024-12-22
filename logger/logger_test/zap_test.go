6b96fb35f5e7a9b9ac2843ce155e74f843c7698e
mzzsfy <mzzsfy@163.com>
2024-12-23 00:47:58 +0800

    Distinct 等方法改为流式

--- logger/logger_test/zap_test.go
+++ logger/logger_test/zap_test.go
@@ -1,7 +1,9 @@
 package logger_test
 
 import (
+    "github.com/mzzsfy/go-util/helper"
     "go.uber.org/zap"
+    "go.uber.org/zap/zapcore"
     "os"
     "testing"
     "time"
@@ -10,14 +12,14 @@ import (
 func Benchmark_Zap(b *testing.B) {
     os.Remove("zap.log")
     time.Sleep(time.Second)
-    logger, _ := zap.Config{
-        Level:            zap.NewAtomicLevelAt(zap.InfoLevel),
-        Development:      false,
-        Encoding:         "json",
-        EncoderConfig:    zap.NewProductionEncoderConfig(),
-        OutputPaths:      []string{"zap.log"},
-        ErrorOutputPaths: []string{"zap.log"},
-    }.Build(zap.WithCaller(false))
+    file, _ := os.OpenFile("zap.log", os.O_CREATE, 0666)
+    defer file.Close()
+    c := zapcore.NewCore(
+        zapcore.NewConsoleEncoder(zap.NewProductionEncoderConfig()),
+        zapcore.AddSync(helper.NewAsyncWriter(file)),
+        zap.NewAtomicLevelAt(zap.InfoLevel),
+    )
+    logger := zap.New(c)
     b.ResetTimer()
     for i := 0; i < b.N; i++ {
         logger.Info("test", zap.Int("i", i))
@@ -27,14 +29,14 @@ func Benchmark_Zap(b *testing.B) {
 func Benchmark_Concurrent_Zap(b *testing.B) {
     os.Remove("zap.log")
     time.Sleep(time.Second)
-    logger, _ := zap.Config{
-        Level:            zap.NewAtomicLevelAt(zap.InfoLevel),
-        Development:      false,
-        Encoding:         "json",
-        EncoderConfig:    zap.NewProductionEncoderConfig(),
-        OutputPaths:      []string{"zap.log"},
-        ErrorOutputPaths: []string{"zap.log"},
-    }.Build(zap.WithCaller(false))
+    file, _ := os.OpenFile("zap.log", os.O_CREATE, 0666)
+    defer file.Close()
+    c := zapcore.NewCore(
+        zapcore.NewConsoleEncoder(zap.NewProductionEncoderConfig()),
+        zapcore.AddSync(helper.NewAsyncWriter(file)),
+        zap.NewAtomicLevelAt(zap.InfoLevel),
+    )
+    logger := zap.New(c)
     b.ResetTimer()
     b.RunParallel(func(pb *testing.PB) {
         for i := 0; pb.Next(); i++ {
